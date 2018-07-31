04e07576927b26b715fb42b37fd9efc1ea75aed3
Remy Zulauff <rzu@open-net.ch>
2018-07-31 11:36:34 +0200

    [TMP] l10n-switzerland: try to fix this

--- l10n_ch_base_bank/migrations/11.0.1.1.0/post-migration.py
+++ l10n_ch_base_bank/migrations/11.0.1.1.0/post-migration.py
@@ -4,14 +4,14 @@
 # from openupgradelib import openupgrade
 
 
-def reference_type_bvr_to_isr(cr):
-    """Change BVR to ISR in reference_type field"""
-    cr.execute("""
-        UPDATE account_invoice SET reference_type='isr'
-        WHERE reference_type = 'bvr';
-        """)
+# def reference_type_bvr_to_isr(cr):
+#     """Change BVR to ISR in reference_type field"""
+#     cr.execute("""
+#         UPDATE account_invoice SET reference_type='isr'
+#         WHERE reference_type = 'bvr';
+#         """)
 
 
-# @openupgrade.migrate()
-def migrate(env, version):
-    reference_type_bvr_to_isr(env.cr)
+# # @openupgrade.migrate()
+# def migrate(env, version):
+#     reference_type_bvr_to_isr(env.cr)
